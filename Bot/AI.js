9c03123ae47326e5cf8e51626756ffc46ba12644
SimonRollus <simrol260@gmail.com>
2018-05-21 22:44:51 +0200

    version 0

---
+++ Bot/AI.js
@@ -0,0 +1,101 @@
+function Brain(delayAction) {
+    this.result = {pattern: [], score: 0};
+    this.delayAction = delayAction;
+    
+    this.action = function() {
+        while (!Game.over){
+            decision = this.think();
+            this.result.pattern.push(decision);
+            this.result.score = Game.score;
+            this.movement(decision); 
+            sleep(delayAction);
+        }
+    }
+
+    this.think = function () {
+        var decision = Math.floor(Math.random() * 4);
+        return decision;
+    };
+    
+    this.movement = function (direction) {
+        switch (direction) {
+            case 0:
+                fireKeyboardEvent("keydown", 37); //LEFT
+            break;
+            case 1:
+                fireKeyboardEvent("keydown", 38);  //UP  
+            break;
+            case 2:
+                fireKeyboardEvent("keydown", 39); //RIGHT
+            break;
+            case 3:
+            break;
+                fireKeyboardEvent("keydown", 40); // DOWN
+            default:   
+        }   
+    };
+    
+    this.capture = function() {
+        source = Game.grid.cells;
+        data = [];
+    
+        for (var row in source) {
+            temp = [];
+            for (var column in source[row]) {
+                if (source[column][row] === null){
+                    temp.push(0);    
+                }
+                else {
+                    temp.push(source[column][row].value)
+                }
+            }  
+            data.push(temp)
+        }
+    
+        return data;   
+    };
+         
+}
+
+function setupParents(numberOfParents) {
+    parents = [];
+    for(var i = 0; i < numberOfParents; i++) {
+        parents.push(new Brain(0));  
+    }
+    return parents;
+}
+
+function bot() {
+    console.log("Running");
+    all_bots = setupParents(10);
+    for (var bot in all_bots) {
+        console.log("The bot " + bot + " is now playing");
+        all_bots[bot].action();
+        console.log(all_bots[bot].result);
+        Game.restart();
+    }
+}
+
+function fireKeyboardEvent(event, keycode) {
+    var keyboardEvent = document.createEventObject ?
+        document.createEventObject() : document.createEvent("Events");
+
+    if (keyboardEvent.initEvent) {
+        keyboardEvent.initEvent(event, true, true);
+    }
+
+    keyboardEvent.keyCode = keycode;
+    keyboardEvent.which = keycode;
+
+    document.dispatchEvent?document.dispatchEvent(keyboardEvent) 
+                           :document.fireEvent(event, keyboardEvent);
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
